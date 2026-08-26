0abced43d336ee63a3dc0672269e7bddce449ada
Aranya Basu <aranyabasu5@gmail.com>
2026-08-26 17:19:45 +0530

    Time: 3 ms (99.82%), Space: 94.1 MB (90.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,31 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ //Linked list method without stack
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Deque<Integer> stack = new ArrayDeque<>();
 
-        ListNode slow = head, fast = head;
+        // 1. Find the middle
+        ListNode slow = head;
+        ListNode fast = head;
 
         while (fast != null && fast.next != null) {
-            stack.push(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // If list length is odd, skip the middle element
-        if (fast != null) {
-            slow = slow.next;
+        // 2. Reverse the second half
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        while (slow != null && !stack.isEmpty()) {
-            if (slow.val != stack.pop()) {
+        // 3. Compare both halves
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            if (first.val != second.val) {
                 return false;
             }
-            slow = slow.next;
+
+            first = first.next;
+            second = second.next;
         }
 
         return true;
-
     }
 }
